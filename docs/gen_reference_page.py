fb346223b5c024e0bc69eb8c5594bbf88f064c61
Benjamin LeBrun <47835295+benlebrun@users.noreply.github.com>
2025-04-09 11:12:42 -0400

    fix gen_reference_page.py

--- docs/gen_reference_page.py
+++ docs/gen_reference_page.py
@@ -4,7 +4,7 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("genlm_grammar").rglob("*.py")):
+for path in sorted(Path("genlm/grammar").rglob("*.py")):
     if any(part.startswith(".") for part in path.parts):
         continue
 
