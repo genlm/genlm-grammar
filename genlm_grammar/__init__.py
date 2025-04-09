76f1a37caecf82de1f476b3cd622fd15cfc3539c
Benjamin LeBrun <47835295+benlebrun@users.noreply.github.com>
2025-04-09 11:17:21 -0400

    rm genlm_grammar directory

--- genlm_grammar/__init__.py
+++
@@ -1,8 +0,0 @@
-import warnings
-from genlm.grammar import *  # noqa: F403
-
-warnings.warn(
-    "Importing from `genlm_grammar` is deprecated. Please use `genlm.grammar` instead.",
-    DeprecationWarning,
-    stacklevel=2,
-)
